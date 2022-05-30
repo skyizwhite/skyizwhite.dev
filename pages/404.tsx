2e3a2f26c0ab175f34cc70d78b99c772fe25b446
paku <paku@skyizwhite.dev>
2022-05-31 04:13:41 +0900

    Revert "yarn remove react-daisyui (#5)"
    This reverts commit 8e6fe56a88ed054d05f3769628fda488e9614a29.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
 import { ExclamationIcon } from '@heroicons/react/outline';
 import Head from 'next/head';
 import Link from 'next/link';
+import { Alert, Button } from 'react-daisyui';
 
 import type { NextPage } from 'next';
 
@@ -9,16 +10,17 @@ const NotFound: NextPage = () => (
     <Head>
       <title>404 Page Not Found</title>
     </Head>
-    <div className="alert alert-error shadow-lg w-min">
-      <ExclamationIcon className="h-6 md:h-8 w-6 md:w-8" />
+    <Alert icon={<ExclamationIcon className="h-6 md:h-8 w-6 md:w-8" />} status="error" className="w-min">
       <span className="text-lg md:text-2xl font-bold whitespace-nowrap">404 Page Not Found</span>
-    </div>
+    </Alert>
     <p className="text-sm md:text-base text-center my-10">
       <span className="inline-block">ページは削除または移動された可能性があります。</span>
       <span className="inline-block">リンクが間違っていないかお確かめください。</span>
     </p>
     <Link href="/" passHref>
-      <button className="btn btn-primary text-base">トップへ戻る</button>
+      <Button color="primary" className="text-base">
+        トップへ戻る
+      </Button>
     </Link>
   </div>
 );
