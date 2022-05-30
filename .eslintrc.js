b8bfd28c3a4794772e3aa803bff92b7081bb1a9f
paku <paku@skyizwhite.dev>
2022-05-30 13:01:15 +0900

    Fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
-    "plugin:tailwind/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
