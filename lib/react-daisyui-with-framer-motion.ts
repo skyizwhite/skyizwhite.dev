d64c154b034c077a2c5fedc942454d537aaf1a79
paku <paku@skyizwhite.dev>
2022-05-31 16:04:48 +0900

    Test custom motion component

---
+++ lib/react-daisyui-with-framer-motion.ts
@@ -0,0 +1,8 @@
+import { motion } from 'framer-motion';
+import { Alert } from 'react-daisyui';
+
+const Motion = {
+  Alert: motion(Alert),
+};
+
+export default Motion;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { HandIcon } from '@heroicons/react/outline';
 import { NextSeo } from 'next-seo';
-import { Alert } from 'react-daisyui';
 
+import Motion from 'lib/react-daisyui-with-framer-motion';
 import { TopSeoProps } from 'lib/seo';
 
 import type { NextPage } from 'next';
@@ -9,9 +9,16 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => (
   <div className="flex items-center justify-center w-full h-full">
     <NextSeo {...TopSeoProps} />
-    <Alert icon={<HandIcon className="h-5 w-5" />} status="info" className="w-min">
+    <Motion.Alert
+      initial={{ opacity: 0, y: '100px' }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 3, type: 'spring' }}
+      icon={<HandIcon className="h-5 w-5" />}
+      status="info"
+      className="w-min"
+    >
       <span className="font-bold whitespace-nowrap">Hello World</span>
-    </Alert>
+    </Motion.Alert>
   </div>
 );
 
