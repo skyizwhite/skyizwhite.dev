dd243e92645db11ae6e01c37bacf344863ea8b7d
paku <paku@skyizwhite.dev>
2022-06-06 05:14:53 +0900

    Customize style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,44 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   safelist: [
     {
-      pattern: /./
+      pattern: /./,
     },
   ],
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
-    themes: ["light", "dark"],
-    darkTheme: "dark",
+    themes: [
+      {
+        custom: {
+          'primary': '#c084fc',
+          'secondary': '#818cf8',
+          'accent': '#bef264',
+          'neutral': '#6b7280',
+          'base-100': '#374151',
+          'info': '#38bdf8',
+          'success': '#5eead4',
+          'warning': '#fbbf24',
+          'error': '#fb7185',
+        },
+      },
+    ],
+  },
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: '716px',
+        md: '740px',
+        lg: '960px',
+        xl: '1024px',
+      },
+    },
+    screens: {
+      sm: '576px',
+      md: '890px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    textTransform: 'normal-case',
   },
 };
