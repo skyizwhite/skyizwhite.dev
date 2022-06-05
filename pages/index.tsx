db35d8392322ef76ed5dc568c444bc4d4bd27095
paku <paku@skyizwhite.dev>
2022-06-06 02:32:20 +0900

    Uninstall framer-motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { HandIcon } from '@heroicons/react/outline';
-import { motion } from 'framer-motion';
 import { NextSeo } from 'next-seo';
 import { Alert } from 'react-daisyui';
 
@@ -10,11 +9,9 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => (
   <div className="flex items-center justify-center w-full h-full">
     <NextSeo {...TopSeoProps} />
-    <motion.div initial={{ opacity: 0, y: '50px' }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
-      <Alert icon={<HandIcon className="h-5 w-5" />} status="info" className="w-min">
-        <span className="font-bold whitespace-nowrap">Hello World</span>
-      </Alert>
-    </motion.div>
+    <Alert icon={<HandIcon className="h-5 w-5" />} status="info" className="w-min">
+      <span className="font-bold whitespace-nowrap">Hello World</span>
+    </Alert>
   </div>
 );
 
