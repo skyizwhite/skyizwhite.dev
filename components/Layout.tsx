d91d51fac84b45a9751a23b5b049f38133021e12
paku <paku@skyizwhite.dev>
2022-06-07 19:13:23 +0900

    Add hamburger menu

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,6 @@
-import { Navbar } from 'react-daisyui';
+import { MenuIcon } from '@heroicons/react/outline';
+import { useRouter } from 'next/router';
+import { Dropdown, Navbar } from 'react-daisyui';
 
 import Container from 'components/Container';
 import NextImage from 'components/NextImage';
@@ -19,29 +21,54 @@ const contents = [
   { href: '/contact', title: 'contact' },
 ];
 
-const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <div className="h-screen flex flex-col">
-    <Navbar className="fixed bg-base-100 border-b border-border-base-300">
-      <Container>
-        <Navbar.Start>
-          <NextLink href="/">
-            <NextImage src={logo} alt="skyizwhite" />
-          </NextLink>
-        </Navbar.Start>
-        <Navbar.End className="px-2 mx-2 hidden sm:flex">
-          {contents.map((content) => (
-            <NextLink key={content.href} href={content.href} className="mx-4 hover:text-primary">
-              {content.title}
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+
+  return (
+    <div className="h-screen flex flex-col">
+      <Navbar className="fixed bg-base-100 border-b border-border-base-300">
+        <Container>
+          <Navbar.Start>
+            <NextLink href="/">
+              <NextImage src={logo} alt="skyizwhite" />
             </NextLink>
-          ))}
-        </Navbar.End>
-      </Container>
-    </Navbar>
-    <div className="flex-1 mt-16">{children}</div>
-    <div className="py-6 w-100 flex items-center justify-center border-t border-base-300">
-      <span>Copyright © 2022 skyizwhite</span>
+          </Navbar.Start>
+          <Navbar.End className="px-2 mx-2 hidden sm:flex">
+            {contents.map((content) => (
+              <NextLink key={content.href} href={content.href} className="mx-4 hover:text-primary">
+                {content.title}
+              </NextLink>
+            ))}
+          </Navbar.End>
+          <Navbar.End className="flex sm:hidden ">
+            <Dropdown horizontal="center">
+              <Dropdown.Toggle color="ghost">
+                <MenuIcon className="w-5 h-5" />
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="w-52 right-0 bg-white">
+                {contents.map((content) => (
+                  <Dropdown.Item
+                    key={content.href}
+                    href={content.href}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      void router.push(content.href);
+                    }}
+                  >
+                    {content.title}
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Navbar.End>
+        </Container>
+      </Navbar>
+      <div className="flex-1 mt-16">{children}</div>
+      <div className="py-6 w-100 flex items-center justify-center border-t border-base-300">
+        <span>Copyright © 2022 skyizwhite</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Layout;
