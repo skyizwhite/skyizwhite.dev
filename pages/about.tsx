a133de251b06ea67ddb8bcfeabc6cacb5cfaeec6
paku <paku@skyizwhite.dev>
2022-06-22 21:03:24 +0900

    Update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import { AcademicCapIcon, BadgeCheckIcon, BriefcaseIcon, IdentificationIcon, ThumbUpIcon } from '@heroicons/react/outline';
+import { AcademicCapIcon, BadgeCheckIcon, BriefcaseIcon, IdentificationIcon } from '@heroicons/react/outline';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
 import { Breadcrumbs } from 'react-daisyui';
@@ -31,9 +31,8 @@ const About: NextPage = () => (
           <h2 className="text-xl sm:text-3xl font-bold">Profile</h2>
         </div>
         <ol className="text-sm ml-7 mt-2 sm:mt-4 list-disc">
-          <li>he/him</li>
-          <li>茨城出身・在住</li>
-          <li>ラーメンとサブカルチャーを愛するオタクです</li>
+          <li>茨城在住</li>
+          <li>サブカルチャーを愛するオタクです</li>
           <li>
             GitHub:{' '}
             <a className="link link-primary" href="https://github.com/skyizwhite">
@@ -53,7 +52,7 @@ const About: NextPage = () => (
             <span className="block">小山工業高等専門学校 電気電子創造工学科</span>
           </li>
           <li>
-            <span className="block">2021/04 - now</span>
+            <span className="block">2021/04 - 現在</span>
             <span className="block">筑波大学 情報学群 情報メディア創成学類</span>
           </li>
         </ol>
@@ -65,7 +64,7 @@ const About: NextPage = () => (
         </div>
         <ol className="text-sm ml-7 mt-2 sm:mt-4 list-disc">
           <li>
-            <span className="block">2021/05 - now</span>
+            <span className="block">2021/05 - 現在</span>
             <span className="block">Enigol Inc. - フロントエンドエンジニア（アルバイト）</span>
           </li>
         </ol>
@@ -76,18 +75,7 @@ const About: NextPage = () => (
           <h2 className="text-xl sm:text-3xl font-bold">Qualification</h2>
         </div>
         <ol className="text-sm ml-7 mt-2 sm:mt-4 list-disc">
-          <li>基本情報技術者（FE）</li>
-        </ol>
-      </ContentBox>
-      <ContentBox className="shadow">
-        <div className="flex items-center">
-          <ThumbUpIcon className="w-5 h-5 sm:w-8 sm:h-8 mr-2" />
-          <h2 className="text-xl sm:text-3xl font-bold">Skills</h2>
-        </div>
-        <ol className="text-sm ml-7 mt-2 sm:mt-4 list-disc">
-          <li>HTML/CSS (Sass, Bootstrap, Tailwind CSS, daisyUI)</li>
-          <li>JavaScript/TypeScript (React, Recoil, Next.js, Vue, Nuxt.js)</li>
-          <li>Common Lisp</li>
+          <li>基本情報技術者</li>
         </ol>
       </ContentBox>
     </div>
